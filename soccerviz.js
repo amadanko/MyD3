466ea55c479db7d6f80e71302342929c7f0a12ea
amadanko <amadanko@gmail.com>
2019-05-08 18:33:59 +0200

    text-size increase on.mouseover

--- soccerviz.js
+++ soccerviz.js
@@ -21,17 +21,20 @@ function createSoccerViz() {
 
       teamG
         .append("circle")
-        .attr("r",20)
-        .style("fill", "pink")
-        .style("stroke", "black")
-        .style("stroke-width", "1px");
+        .attr("r",0)
+        .transition()
+        .delay(function(d,i) {return i * 100})
+        .duration(500)
+        .attr("r", 40)
+        .transition()
+        .duration(500)
+        .attr("r",20);
 
       teamG
         .append("text")
         .style("text-anchor", "middle")
         .attr("y", 30)
-        .style("font-size","10px")
-        .text(function(d) {return d.team;});
+          .text(d => d.team)
 
       var dataKeys = d3.keys(incomingData[0]).filter(function(el) {
         return el != "team" && el !="region";
@@ -51,12 +54,47 @@ function createSoccerViz() {
       var radiusScale = d3.scaleLinear()
         .domain([0,maxValue]).range([2,20]);
 
-      d3.selectAll("g.overallG").select("circle")
+      d3.selectAll("g.overallG").select("circle").transition().duration(1000)
         .attr("r", function(d) {
           return radiusScale(d[datapoint]);
         });
 
       };
+
+/*
+      teamG.on("mouseover", highlightRegion);
+      function highlightRegion(d) {
+        d3.selectAll("g.overallG").select("circle")
+          .style("fill", function(p) {
+            return p.region == d.region ? "red" : "gray";
+          });
+      }
+*/
+    teamG.on("mouseover", highlightRegion);
+
+      function highlightRegion (d) {
+         d3.select(this).select("text").classed("active", true).attr("y", 10);
+
+         d3.selectAll("g.overallG").select("circle")
+           .each(function (p) {
+             p.region == d.region ?
+               d3.select(this).classed("active", true) :
+               d3.select(this).classed("inactive", true);
+           });
+
+         this.parentElement.appendChild(this);
+       };
+
+
+       teamG.on("mouseout", unHighlight);
+
+               function unHighlight() {
+                 d3.selectAll("g.overallG").select("circle").attr("class", "");
+                 d3.selectAll("g.overallG").select("text")
+                   .classed("active", false).attr("y", 30);
+               };
+
+
 
       }
 
